08b8e596ddd339f518685acddf687d738ab16126
AmritPriyadarshee <amrit.p@live.ca>
2017-10-14 20:53:34 -0400

    Add files via upload

---
+++ Go-Back-N/gbnreceiver.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+'''GBN Receiver'''
+
+import struct
+import sys
+import socket
+
+'''Create a socket and bind to the OS assigned port'''
+def create_socket():
+    sock = socket.socket(socket.AF_INET,
+                        socket.SOCK_DGRAM) # UDP
+    sock.bind(('', 0))
+
+    '''Write to file for channel setup'''
+    f = open('recvInfo', 'w')
+    hostname = sock.getsockname()
+    f.write(socket.gethostname() + " " + str(hostname[1]))
+    f.close()
+
+    '''Setup file for writing data to'''
+    f = open(sys.argv[1], 'w') 
+
+    '''Ready to receive'''
+    expected_seq_num = 0
+    while True:
+    	data, address = sock.recvfrom(512)
+    	udp_header = struct.unpack('!III', data[0:12])
+
+	'''Check Packet Sequence Number'''
+	if (udp_header[2] == expected_seq_num):
+
+		'''Receive EOT Packet'''
+		if (udp_header[0] == 2):
+
+			packet_type = 2
+			packet_length = 12
+			packet_sequence_num = 99999
+			print("PKT RECV EOT %d %d" % (udp_header[1], udp_header[2]))
+			eot_header = struct.pack('!III', packet_type, packet_length, packet_sequence_num)
+			sock.sendto(eot_header, (address[0], int(address[1])))
+			print("PKT SEND EOT %d %d" % (packet_length, packet_sequence_num))
+			'''Close the socket'''
+			print("Closing socket")
+			sock.close()
+			f.close()
+			return
+
+		'''Receive Data Packet'''
+		if (udp_header[0] == 0):
+			f.write(data[12:])
+			'''Create ACK'''
+			packet_type = 1
+			packet_length = 12
+			packet_sequence_num = udp_header[2]
+			print("PKT RECV DAT %d %d" % (udp_header[1], udp_header[2]))
+			ack_header = struct.pack('!III', packet_type, packet_length, packet_sequence_num)
+			sock.sendto(ack_header, (address[0], int(address[1])))
+			print("PKT SEND ACK %d %d" % (packet_length, packet_sequence_num))
+			expected_seq_num+=1
+
+	if (udp_header[0] == 2):
+		print("PKT RECV EOT %d %d" % (udp_header[1], udp_header[2]))
+		packet_type = 2
+		packet_length = 12
+		packet_sequence_num = 99999
+		eot_header = struct.pack('!III', packet_type, packet_length, packet_sequence_num)
+		sock.sendto(eot_header, (address[0], int(address[1])))
+		print("PKT SEND EOT %d %d" % (packet_length, packet_sequence_num))
+		'''Close the socket'''
+		sock.close()
+		f.close()
+		return
+	else:
+		'''Resend most recent ACK'''
+		packet_type = 1
+		packet_length = 12
+		packet_sequence_num = (expected_seq_num - 1)
+		ack_header = struct.pack('!III', packet_type, packet_length, packet_sequence_num)
+		sock.sendto(ack_header, (address[0], int(address[1])))
+		print("PKT SEND ACK %d %d" % (packet_length, packet_sequence_num))
+
+create_socket()
+
---
+++ Go-Back-N/gbnsender.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python
+import socket
+import struct
+import sys
+import time
+
+sequence_num = 0
+window = []
+window_size = 10
+base = 0
+
+f = open(sys.argv[2], 'rb')
+data = None
+done = False
+
+channelInfo = open('channelInfo', 'r')
+x = channelInfo.read()
+y = x.split()
+hostname = y[0]
+port = y[1]
+
+mostrecentAck = time.time()
+
+timeout = float(sys.argv[1])
+timeout = float(timeout/1000)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind(('', 0))
+s.settimeout(timeout)
+while not done or window:
+	'''Go through 500 byte segments and send'''
+	'''While window isn't full and not EOF''' ##TODO: Add EOF CONDITION to while loop
+	if (sequence_num < window_size + base) and (data != ""):
+		data = f.read(500)
+		packet_type = 0
+		packet_length = len(data) + 12
+		packet_sequence_num = sequence_num
+
+		udp_header = struct.pack('!III', packet_type, packet_length, packet_sequence_num)
+		s.sendto(udp_header + data, (str(hostname), int(port)))
+
+		print("PKT SEND DAT %d %d" % (packet_length, packet_sequence_num))
+		sequence_num = (sequence_num + 1)		
+		
+		'''Add the packet we just sent to the window'''
+		window.append(udp_header + data)
+		
+		'''If we're done reading, exit the loop by setting the boolean to be True'''
+		if (not data):
+			done = True
+			'''Send EOT packet'''
+			packet_type = 2
+			packet_length = 12
+			packet_sequence_num = sequence_num
+			eot_header = struct.pack('!III', packet_type, packet_length, packet_sequence_num)
+			s.sendto(eot_header, (str(hostname), int(port)))
+			sequence_num= (sequence_num + 1)
+
+			print("PKT SEND EOT %d %d" % (packet_length, packet_sequence_num))
+			'''Add the packet we just sent to the window'''
+			window.append(udp_header + data)
+			data = f.read(500)
+
+	try:
+		data, address = s.recvfrom(12)
+		udp_header = struct.unpack('!III', data[0:12])
+		rec_packet_type = udp_header[0]
+		rec_packet_length = udp_header[1]
+		rec_packet_sequence_num = udp_header[2]
+		if (rec_packet_sequence_num == 99999):
+			print("PKT RCV EOT %d %d" % (rec_packet_length, rec_packet_sequence_num))
+			window = []
+			s.close()
+			f.close()
+
+		while (rec_packet_sequence_num >= base) and window:
+			print("PKT RECV ACK 12 %d" % rec_packet_sequence_num)
+
+			lwindow = window[0]
+			lwindow_header = struct.unpack('!III', lwindow[0:12])
+			lwindow_seq = lwindow_header[2]
+
+			while (lwindow_seq <= rec_packet_sequence_num):
+				mostrecentAck = time.time()
+				
+				todel = window[0]
+				todel_h = struct.unpack('!III', lwindow[0:12])
+				del window[0]
+				base= (base + 1)
+
+				'''Annoying way of updating stuff b/c Python wasn't being normal'''
+				lwindow = window[0]
+				lwindow_header = struct.unpack('!III', lwindow[0:12])
+				lwindow_seq = lwindow_header[2]
+
+	except:
+		if (time.time() - mostrecentAck > timeout):
+			for i in range(len(window)):
+				x = window[i]
+				resend_packet = struct.unpack('!III', x[0:12])
+				print("PKT SEND DAT %d %d" % (resend_packet[1], resend_packet[2]))
+				s.sendto(window[i], (str(hostname), int(port)))
+	
